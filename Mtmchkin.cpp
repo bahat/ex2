4899f24161c7b55361308603a4ec9721bd36f037
Omer Bahat <omer@Omers-MacBook-Pro-2.local>
2022-12-06 12:32:02 +0200

    implemented mtmchkin functions

---
+++ Mtmchkin.cpp
@@ -0,0 +1,55 @@
+//
+// Created by Omer Bahat on 05/12/2022.
+//
+#include "Mtmchkin.h"
+
+Mtmchkin::Mtmchkin(const char *playerName, const Card *cardsArray, int numOfCards):
+        m_player(playerName),
+        m_numOfCards(numOfCards),
+        m_cardsArray(cardsArray),
+        m_currentCardIndex(0)
+{
+}
+
+void Mtmchkin::playNextCard()
+{
+    if(!isOver())
+    {
+        if(m_currentCardIndex>=m_numOfCards)
+        {
+            m_currentCardIndex=0;
+        }
+        Card currentCard = m_cardsArray[m_currentCardIndex];
+        currentCard.Card::printInfo();
+        currentCard.applyEncounter(m_player);
+        m_player.printInfo();
+        m_currentCardIndex++;
+    }
+}
+
+bool Mtmchkin::isOver() const
+{
+    if(getGameStatus()==GameStatus::MidGame)
+    {
+        return false;
+    }
+    return true;
+}
+
+GameStatus Mtmchkin::getGameStatus() const {
+    if(m_player.getLevel()==Player::getMaxLevel())
+    {
+        //player won
+        return GameStatus::Win;
+    }
+    if(m_player.isKnockedOut())
+    {
+        //player lost
+        return GameStatus::Loss;
+    }
+    else
+    {
+        return GameStatus::MidGame;
+    }
+}
+
